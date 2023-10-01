295b99af4570e5f2c9bedc11b5a488ef81e4e4f3
Joud Shaieb <joudshaieb524@gmail.com>
2023-10-01 15:38:14 +0400

    cln

--- src/services/MailSender.cs
+++ src/services/MailSender.cs
@@ -6,36 +6,20 @@ using CustomModels;
 
 class MailServices
 {
-    // private string? SmtpServer {get; set;}
-    // private int SmtpPort {get; set;} 
-    // private string? FromEmail {get; set;} 
-    // private string? ToEmail {get; set;} 
-    // private string? Username {get; set;} 
-    // private string? Password {get; set;} //"xxxyneunykzuiend"; 
-
-    // public MailServices(string smtpServer, int smtpPort, 
-    //     string fromEmail, string toEmail, string username, string password)
-    // {
-    //     this.SmtpServer = smtpServer;
-    //     this.SmtpPort = smtpPort;
-    //     this.FromEmail = fromEmail;
-    //     this.ToEmail = toEmail;
-    //     this.Username = username;
-    //     this.Password = password;
-    // }   
-
     public static void SendEmail(MailOptions mOptions)
     {
-        // Email message
-        MailMessage mail = new MailMessage(mOptions.FromEmail, mOptions.ToEmail);
-        mail.Subject = "Windows Startup Notification";
-        mail.Body = "Windows has started up.";
+        MailMessage mail = new MailMessage(mOptions.FromEmail, mOptions.ToEmail)
+        {
+            Subject = "Windows Startup Notification",
+            Body = "Windows has started up."
+        };
 
-        // SMTP client
-        SmtpClient smtpClient = new SmtpClient(mOptions.SmtpServer);
-        smtpClient.Port = mOptions.SmtpPort;
-        smtpClient.Credentials = new NetworkCredential(mOptions.Username, mOptions.Password);
-        smtpClient.EnableSsl = true; // Use SSL for secure connection
+        SmtpClient smtpClient = new SmtpClient(mOptions.SmtpServer)
+        {
+            Port = mOptions.SmtpPort,
+            Credentials = new NetworkCredential(mOptions.Username, mOptions.Password),
+            EnableSsl = true // Use SSL for secure connection
+        };
 
         try
         {
