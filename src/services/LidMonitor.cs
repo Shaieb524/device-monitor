c462c129fcd4ea3cebcd58e7e8041e3a9b7319da
Joud Shaieb <joudshaieb524@gmail.com>
2023-10-10 01:07:41 +0400

    write to txt

--- src/services/LidMonitor.cs
+++ src/services/LidMonitor.cs
@@ -4,6 +4,7 @@ using Microsoft.Win32;
 class LidMonitor
 {
     private static MailOptions _mailOptions;
+    private const string LOG_FILE_PATH = "logs/user_activity.txt";
 
     public static void MainCall(MailOptions mailOptions)
     {
@@ -24,11 +25,48 @@ class LidMonitor
         if (e.Reason == SessionSwitchReason.SessionLock)
         {
             Console.WriteLine("I left my desk");
+            EnsureDirectoryExists(GetFullFilePath(LOG_FILE_PATH));
+            AppendMessageToFile(GetFullFilePath(LOG_FILE_PATH), "Desktop closed!");
         }
         else if (e.Reason == SessionSwitchReason.SessionUnlock)
         {
             Console.WriteLine("I returned to my desk");
+            EnsureDirectoryExists(GetFullFilePath(LOG_FILE_PATH));
+            AppendMessageToFile(GetFullFilePath(LOG_FILE_PATH), "Desktop opened!");
+        }
+    }
 
+    static void AppendMessageToFile(string filePath, string message)
+    {
+        try
+        {
+            using (StreamWriter writer = new StreamWriter(filePath, true))
+            {
+                writer.WriteLine($"{ DateTime.Now + " : " + message}");
+            }
+        } 
+        catch (Exception ex) 
+        {
+            Console.WriteLine("An error occurred while writing to the file: " + ex.Message);
+        }
+    }
+
+    static string GetFullFilePath(string relativeFilePath)
+    {
+        string projectRootPath = Directory.GetCurrentDirectory();
+        string assemblyDirectory = Path.GetDirectoryName(System.Reflection.Assembly.GetExecutingAssembly().Location);
+        var tt = System.AppDomain.CurrentDomain.BaseDirectory;
+        var rootDir = System.IO.Path.GetDirectoryName(System.Reflection.Assembly.GetExecutingAssembly().CodeBase);
+        return Path.Combine(assemblyDirectory, relativeFilePath);
+    }
+
+    static void EnsureDirectoryExists(string filePath)
+    {
+        string directoryPath = Path.GetDirectoryName(filePath);
+
+        if (!Directory.Exists(directoryPath))
+        {
+            Directory.CreateDirectory(directoryPath);
         }
     }
 }
